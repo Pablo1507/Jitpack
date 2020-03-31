009a08ca122c2569994ccc1b83c66a573748e752
Juan <>
2020-03-31 11:47:53 +0200

    Proyecto Maven subido

---
+++ CopyrightPablo/src/main/java/Copyright.java
@@ -0,0 +1,12 @@
+public class Copyright {
+    public String getText(){
+        return "\n" +
+                "        Copyright (C) 1999 Pablo Gutiérrez Campos - All Rights Reserved\n" +
+                "        You may use, distribute and modify this code under the\n" +
+                "        terms of the XYZ license, which unfortunately won't be\n" +
+                "        written for another century.\n" +
+                "        \n" +
+                "        You should have received a copy of the XYZ license with\n" +
+                "        this file. If not, please visit : https://github.com/Pablo1507/Jitpack.git";
+    }
+}
